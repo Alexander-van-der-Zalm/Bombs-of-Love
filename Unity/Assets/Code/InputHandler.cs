19ef346319c52d123c765e284d77d4f15e6c1662
Alexander <bahamutho@gmail.com>
2016-07-03 10:40:55 +0200

    Xbox plugin

--- Unity/Assets/Code/InputHandler.cs
+++ Unity/Assets/Code/InputHandler.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using XInputDotNetPure;
 
 [RequireComponent(typeof(MovementPhysics))]
 public class InputHandler : MonoBehaviour
 {
-    public KeyCode Left, Right, Up, Down;
+    public KeyCode Up = KeyCode.W;
+    public KeyCode Left = KeyCode.A;
+    public KeyCode Down = KeyCode.S;
+    public KeyCode Right = KeyCode.D;
+
+
+
+    public PlayerIndex XboxControllerIndex;
 
     private MovementPhysics physics;
+    private GamePadState prevState;
+    private GamePadState state;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         physics = GetComponent<MovementPhysics>();
 	}
@@ -17,6 +27,8 @@ public class InputHandler : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-	    
-	}
+        prevState = state;
+        state = GamePad.GetState(XboxControllerIndex);
+        
+    }
 }
