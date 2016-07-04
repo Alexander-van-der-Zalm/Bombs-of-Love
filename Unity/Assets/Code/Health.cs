c8087f6cf49c5fa6e0ab5e26c76336b9aeb939c8
Alexander <bahamutho@gmail.com>
2016-07-04 11:37:41 +0200

    Death animation

--- Unity/Assets/Code/Health.cs
+++ Unity/Assets/Code/Health.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class Health : MonoBehaviour
 {
+    public int MaxAmount = 1;
     public int Amount = 1;
-    
+    public bool IsDead = false;
+
+   
     public void DoDamge(int damage)
     {
         Amount -= damage;
@@ -16,5 +19,12 @@ public class Health : MonoBehaviour
     public void Die()
     {
         Debug.Log("You Dead Son");
+        IsDead = true;
+    }
+
+    public void Respawn()
+    {
+        IsDead = false;
+        Amount = MaxAmount;
     }
 }
--- Unity/Assets/Code/PlayerAnimationHandler.cs
+++ Unity/Assets/Code/PlayerAnimationHandler.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerAnimationHandler : MonoBehaviour
 {
     private int deathTriggerH = Animator.StringToHash("Death");
+    private int respawnTriggerH = Animator.StringToHash("Respawn");
     private int velocityH = Animator.StringToHash("Velocity");
     private int velocityH_X = Animator.StringToHash("Velocity_X");
     private int velocityH_Y = Animator.StringToHash("Velocity_Y");
@@ -11,6 +12,7 @@ public class PlayerAnimationHandler : MonoBehaviour
     private Animator anim;
     private Rigidbody2D rb;
     private MovementPhysics physics;
+    private Health health;
 
     // Use this for initialization
     void Start ()
@@ -18,6 +20,7 @@ public class PlayerAnimationHandler : MonoBehaviour
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         physics = GetComponent<MovementPhysics>();
+        health = GetComponent<Health>();
     }
 	
 	// Update is called once per frame
@@ -27,10 +30,20 @@ public class PlayerAnimationHandler : MonoBehaviour
         anim.SetFloat(velocityH, rb.velocity.magnitude/physics.Speed);
         anim.SetFloat(velocityH_X, v.x);
         anim.SetFloat(velocityH_Y, v.y);
+
+        if(health.IsDead)
+        {
+            Death();
+        }
     }
 
     public void Death()
     {
         anim.SetTrigger(deathTriggerH);
+    }
+
+    public void Respawn()
+    {
+        anim.SetTrigger(respawnTriggerH);
     }
 }
