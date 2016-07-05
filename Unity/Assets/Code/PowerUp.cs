a5e8bde9ef1665dfe78e7321ea9df651292ef583
Alexander <bahamutho@gmail.com>
2016-07-05 12:22:38 +0200

    PowerUp code

---
+++ Unity/Assets/Code/PowerUp.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PowerUp : MonoBehaviour
+{
+    public enum PowerUpTypes
+    {
+        BombRange,
+        MoreBombs
+    }
+
+    public PowerUpTypes Type;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // Only players can pick up power ups
+        if (other.GetComponent<Player>() == null)
+            return;
+
+        PowerUpPlayer(other.gameObject);
+
+        CleanUp();
+    }
+
+    private void PowerUpPlayer(GameObject player)
+    {
+        Bomber bomber = player.GetComponent<Bomber>();
+        switch(Type)
+        {
+            default:
+            case PowerUpTypes.BombRange:
+                bomber.BonusRange++;
+                return;
+            case PowerUpTypes.MoreBombs:
+                bomber.AvailableBombs++;
+                bomber.MaxBombs++;
+                return;
+        }
+    }
+
+    private void CleanUp()
+    {
+        // PickUpSound Trigger
+        GameObject.Destroy(this.gameObject);
+    }
+}
